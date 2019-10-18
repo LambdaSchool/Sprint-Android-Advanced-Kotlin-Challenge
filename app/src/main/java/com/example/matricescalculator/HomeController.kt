aaec5aa8867a93446259c2fbf83ad4ff2c7e7822
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2019-10-18 10:58:58 -0600

    add Home Controller

--- app/src/main/java/com/example/matricescalculator/HomeController.kt
+++ app/src/main/java/com/example/matricescalculator/HomeController.kt
@@ -1,2 +1,40 @@
 package com.example.matricescalculator
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bluelinelabs.conductor.Controller
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+
+class HomeController : Controller() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
+
+        val view = inflater.inflate(R.layout.activity_main, container, false)
+
+
+
+        val etA11 = view.one.text.toString()
+
+        val etA12 = view.two.text.toString()
+
+        val etA21 = view.three.text.toString()
+
+        val etA22 = view.four.text.toString()
+
+        val etB11 = view.five.text.toString()
+
+        val etB12 = view.six.text.toString()
+
+        val etB21 = view.seven.text.toString()
+
+        val etB22 = view.eight.text.toString()
+
+
+
+        return view
+
+    }
+
+}
--- app/src/main/java/com/example/matricescalculator/MainActivity.kt
+++ app/src/main/java/com/example/matricescalculator/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.matricescalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import com.bluelinelabs.conductor.Conductor
+import com.bluelinelabs.conductor.Router
+import com.bluelinelabs.conductor.RouterTransaction
 import kotlinx.android.synthetic.main.activity_main.*
 import java.math.BigDecimal
 
@@ -9,10 +13,28 @@ import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var router: Router
+
+    private val container: ViewGroup by lazy {
+
+        this.findViewById<ViewGroup>(R.id.layout_home_controller)
+
+    }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        router = Conductor.attachRouter(this, container, savedInstanceState)
+        if (!router.hasRootController()) {
+            router.setRoot(RouterTransaction.with(HomeController()))
+        }
+    }
+    override fun onBackPressed() {
+        if (!router.handleBack()) {
+            super.onBackPressed()
+        }
     }
 }
