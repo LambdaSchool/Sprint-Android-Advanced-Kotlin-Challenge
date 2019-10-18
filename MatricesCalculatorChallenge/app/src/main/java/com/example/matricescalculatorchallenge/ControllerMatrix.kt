3730d16dbe7780e7b8675a84603c57e0a558bc69
ronniedipple <ronniedipple@gmail.com>
2019-10-18 12:25:37 -0500

    Main controller matrix layout complete

--- MatricesCalculatorChallenge/app/src/main/java/com/example/matricescalculatorchallenge/ControllerMatrix.kt
+++ MatricesCalculatorChallenge/app/src/main/java/com/example/matricescalculatorchallenge/ControllerMatrix.kt
@@ -6,9 +6,12 @@ import android.view.ViewGroup
 import com.bluelinelabs.conductor.Controller
 
 class ControllerMatrix: Controller() {
+
+
+  var array = Array(2) { Array(2) { 0 } }
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    val view = inflater.inflate(R.layout.main_controller_matrix_layout, container, false)
+    return view
   }
 
-  var array = Array(4) { Array(4) { 0 } }
 }
--- MatricesCalculatorChallenge/app/src/main/java/com/example/matricescalculatorchallenge/ControllerTwoMatrices.kt
+++
@@ -1,4 +0,0 @@
-package com.example.matricescalculatorchallenge
-
-class ControllerTwoMatrices {
-}
