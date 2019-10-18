be06c8525866701ef7f6a6ed6d46bd3cd3aa3293
vdtyson <versilistyson@gmail.com>
2019-10-18 16:33:31 -0400

    Matrix

--- Sprint/app/src/main/java/com/versilis/additionalandroidsprint/Matrix.kt
+++ Sprint/app/src/main/java/com/versilis/additionalandroidsprint/Matrix.kt
@@ -1,6 +1,27 @@
 package com.versilis.additionalandroidsprint
 
-class Matrix(val x: Int,val y: Int, val arrary: Array<Array<Int>>) {
+import java.util.*
 
-    val array = Array(x) { Array(y) {0} }
+class Matrix(private val x: Int,private val y: Int, val arrary: Array<Array<Int>>) {
+
+    private var TwoDArray = Array(y) { Array(x) {0} }
+
+    operator fun Matrix.times(other: Matrix): Matrix {
+        val rows1 = this.x
+        val cols1 = this.y
+        val rows2 = other.x
+        val cols2 = other.y
+        require(cols1 == rows2)
+       /* val result = Matrix(rows1) { Vector(cols2) }
+        for (i in 0 until rows1) {
+            for (j in 0 until cols2) {
+                for (k in 0 until rows2) {
+                    result[i][j] += this[i][k] * other[k][j]
+                }
+            }
+        }*/
+        return result
+    }
+
+
 }
